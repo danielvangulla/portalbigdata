db0ecb1ec25f96156560249ab6c3a74a37aafb09
Daniel Van Gulla <daniel.gulla88@gmail.com>
2018-04-11 17:39:02 +0800

    test

--- resources/views/content/dikomando.blade.php
+++ resources/views/content/dikomando.blade.php
@@ -14,7 +14,7 @@
 
       <!-- body -->
       <div class="modal-body">
-        <embed id="embed" src="http://36.67.90.85/mioc/vAlpha" width="100%;" height="100%;"/>
+        <embed id="embed" src="http://www.google.co.id" width="100%;" height="100%;"/>
       </div>
       <!-- body -->
 
